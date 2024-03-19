1ab9ffb39e63dd9b6962069e1f5c8a68d380d8cc
Lowxorx <lowxorx@lahan.fr>
2024-03-19 22:09:39 +0000

    Matrixx bringup

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -7,11 +7,11 @@
 # Inherit from mondrian device
 $(call inherit-product, device/xiaomi/mondrian/device.mk)
 
-# Inherit from common aosp configuration
+# Inherit from common lineage configuration
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := evolution_mondrian
+PRODUCT_NAME := lineage_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
 
@@ -28,4 +28,16 @@ BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:12/SKQ1.230401.001/V816.0.3.0
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_BOOT_ANIMATION_RES := 1440
-EXTRA_UDFPS_ANIMATIONS := true
+
+# Matrixx
+MATRIXX_BUILD_TYPE := Unofficial
+MATRIXX_MAINTAINER := Lowxorx
+MATRIXX_CHIPSET := SM8475
+MATRIXX_BATTERY := 5160mah
+MATRIXX_DISPLAY := 1440x3600
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Nuke Auxio
+TARGET_EXCLUDES_AUXIO := true
